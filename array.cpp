9cd39f04f4be03fbf279da701e3a3e609bd2f234
Rishikesh Chauhan <rishikeshchauhan555@gmail.com>
2024-12-18 12:04:07 +0530

    addon

--- array.cpp
+++ array.cpp
@@ -3,12 +3,10 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int A[n];
-    for(int i=0; i<n; i++){
-        cin>>A[i];
-   // }
-   // for(int i=0; i<n; i++){
-        cout<<A[i]<<" ";
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        cout<<arr[i]<<" ";
     }
     return 0;
 }
